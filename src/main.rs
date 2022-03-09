456cc52e23c189a3c8ab3703b3632704a95e26b3
Punyawat Rujipiranunt <rew150@outlook.com>
2022-03-09 17:44:21 +0700

    test ffi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[cfg(all(target_env = "gnu"))]
+#[link(name = "stdc++")]
+extern {
+    fn printf(format: *const c_char) -> libc::c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let string = CString::new("Hello, FFI!\n").unwrap();
+    unsafe {
+        printf(string.as_ptr());
+    }
 }
